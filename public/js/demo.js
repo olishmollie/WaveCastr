5d77e23f19fd89836d22479e99d3288547c5eab2
AJ Bond <olishmollie@gmail.com>
2017-04-19 01:28:17 -0500

    Update demo text for color changes

--- public/js/demo.js
+++ public/js/demo.js
@@ -3,7 +3,7 @@ var Demo = (function() {
     "Welcome to WaveCastr!",
     "You can add additional users by simply copying the url (we've made it easy with the 'Share Link' button) into an incognito window or another browser and enter your name.",
     "Once you're ready, activate everyone's mics with the button below and select yes when your browser asks for permission to access your microphone",
-    "Congrats, your mic is armed and ready to go! Once everyone's permissions have been granted, your guests' icons will turn green.",
+    "Congrats, your mic is armed and ready to go! Once everyone's permissions have been granted, your guests' icons will turn yellow.",
     "Press record, and both browsers will a record separate audio track for each guest. Push stop and those tracks will be automatically uploaded to our AWS server.",
     "Nice! You've recorded your first podcast with WaveCastr! Once the tracks are ready, you should see links to download them below, and your editor can now get started on post-production.",
     "Thanks for trying WaveCastr!"
