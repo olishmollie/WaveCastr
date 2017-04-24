0ea6075861cdfc548f995467d08523c2eddb6f8a
AJ Bond <olishmollie@gmail.com>
2017-04-23 23:18:19 -0500

    Modify auth spec for demo

--- spec/features/auths_spec.rb
+++ spec/features/auths_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Auths", type: :feature do
-  let!(:valid_user) { create(:user) }
-  let!(:invalid_user) { User.new(name: "test", email: "test@test.com", password: "password") }
 
-  scenario "I log in with an invalid email" do
+  scenario "I visit root and am at the demo_user's dashboard" do
     visit "/"
-    login(invalid_user)
-    expect(page).to have_content "Invalid Email or password"
-  end
-
-  scenario "I log in with a valid email" do
-    visit "/"
-    login(valid_user)
-    at_dashboard?
+    expect(page).to have_content("demo_user")
   end
 end
