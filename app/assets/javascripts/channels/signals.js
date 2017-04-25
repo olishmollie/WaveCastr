5aafadeffb328085f17f2e192af7d679222bfbb3
AJ Bond <olishmollie@gmail.com>
2017-04-24 20:44:57 -0500

    Merge branch 'master' into rtc_peer_connection

--- app/assets/javascripts/channels/signals.js
+++ app/assets/javascripts/channels/signals.js
@@ -1,28 +1,25 @@
-$(document).ready(function() {
+function subscribeToSignals() {
+  App.signals = App.cable.subscriptions.create({channel: "SignalsChannel", lobby: lobby}, {
+    connected: function() {
+      // Called when the subscription is ready for use on the server
+    },
 
-var lobby = window.location.pathname.replace(/\/episodes\//, "");
-App.signals = App.cable.subscriptions.create({channel: "SignalsChannel", lobby: lobby}, {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
+    disconnected: function() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received: function(data) {
-    switch (data.type) {
-      case 'offer':
-        if (isHost()) { return; }
-        hostId = data.host_id; 
-        hostSDP = data.sdp;
-        document.dispatchEvent(receiveOffer);
-        break;
-      case 'answer':
-        guestSDP = data.sdp;
-        document.dispatchEvent(receiveAnswer);
+    received: function(data) {
+      switch (data.type) {
+        case 'offer':
+          if (isHost()) { return; }
+          hostId = data.host_id; 
+          hostSDP = data.sdp;
+          document.dispatchEvent(receiveOffer);
+          break;
+        case 'answer':
+          guestSDP = data.sdp;
+          document.dispatchEvent(receiveAnswer);
+      }
     }
-  }
-});
-
-});
+  });
+}
